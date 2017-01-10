3b1e4b1110c8ecba64131447f59f0a52a432e73a
Cédric Ronvel <cedric.ronvel@gmail.com>
2017-01-10 16:02:31 +0100

    Fixing some ball physics

--- sample/ball-test.js
+++ sample/ball-test.js
@@ -7,12 +7,13 @@ var phy = soccer.Phy.create() ;
 
 //console.log( state ) ;
 
-state.ball.bVector.position.z = 10 ;
-console.log( state.ball.bVector ) ;
+//state.ball.bVector.position.z = 10 ;
+state.ball.bVector.vector.y = 20 ;
+console.log( state.ball.bVector , state.ball.accelVector ) ;
 
 for ( var i = 0 ; i < 100 ; i ++ )
 {
 	phy.update( state ) ;
-	console.log( state.ball.bVector ) ;
+	console.log( state.ball.bVector , state.ball.accelVector ) ;
 }
 
