5ac9116f795dce084f2a88056ba146a74362bbff
Cédric Ronvel <cedric.ronvel@gmail.com>
2017-01-12 00:01:41 +0100

    wip

--- sample/ball-test.js
+++ sample/ball-test.js
@@ -5,14 +5,14 @@ var soccer = require( '../lib/coding-soccer.js' ) ;
 var game = soccer.Game.create() ;
 
 console.log( game ) ;
-return ;
-//game.state.ball.bVector.position.z = 10 ;
-//game.state.ball.bVector.vector.y = 20 ;
-console.log( game.state.ball.bVector , game.state.ball.accelVector ) ;
 
-for ( var i = 0 ; i < 100 ; i ++ )
+game.entity.ball.boundVector.position.z = 10 ;
+//game.entity.ball.boundVector.vector.y = 20 ;
+console.log( game.entity.ball.boundVector , game.entity.ball.accelVector ) ;
+
+for ( var i = 0 ; i < 60 ; i ++ )
 {
 	game.update() ;
-	console.log( game.state.ball.bVector , game.state.ball.accelVector ) ;
+	console.log( game.entity.ball.boundVector , game.entity.ball.accelVector ) ;
 }
 
