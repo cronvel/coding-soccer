1f3818bfafec54f798de9e6195df69ab18ae47ab
Cédric Ronvel <cedric.ronvel@gmail.com>
2017-01-14 18:54:53 +0100

    Iiiiiiit wooooooorks!

--- sample/ball-test.js
+++ sample/ball-test.js
@@ -6,13 +6,21 @@ var game = soccer.Game.create() ;
 
 console.log( game ) ;
 
+/*
 game.entity.ball.boundVector.position.z = 10 ;
 game.entity.ball.boundVector.vector.y = 10 ;
-console.log( game.entity.ball.boundVector , game.entity.ball.accelVector ) ;
+//*/
 
-for ( var i = 0 ; i < 470 ; i ++ )
+//*
+game.entity.ball.boundVector.position.z = 0.15 ;
+game.entity.ball.boundVector.vector.y = 10 ;
+//*/
+
+console.log( game.entity.ball.boundVector ) ; //, game.entity.ball.accelVector ) ;
+
+for ( var i = 0 ; i < 100 ; i ++ )
 {
 	game.update() ;
-	console.log( '#' + i + ':' , game.entity.ball.boundVector , game.entity.ball.accelVector ) ;
+	console.log( '#' + i + ':' , game.entity.ball.boundVector ) ; //, game.entity.ball.accelVector ) ;
 }
 
