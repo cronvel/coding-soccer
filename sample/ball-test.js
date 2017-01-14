4619c4c3ecfb9140b56897cc497c04cc384c2c70
Cédric Ronvel <cedric.ronvel@gmail.com>
2017-01-14 17:09:35 +0100

    wip

--- sample/ball-test.js
+++ sample/ball-test.js
@@ -10,9 +10,9 @@ game.entity.ball.boundVector.position.z = 10 ;
 game.entity.ball.boundVector.vector.y = 10 ;
 console.log( game.entity.ball.boundVector , game.entity.ball.accelVector ) ;
 
-for ( var i = 0 ; i < 120 ; i ++ )
+for ( var i = 0 ; i < 470 ; i ++ )
 {
 	game.update() ;
-	console.log( game.entity.ball.boundVector , game.entity.ball.accelVector ) ;
+	console.log( '#' + i + ':' , game.entity.ball.boundVector , game.entity.ball.accelVector ) ;
 }
 
