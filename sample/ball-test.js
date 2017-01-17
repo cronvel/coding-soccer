e59941cbd806391c9b839a1a1a98095a8e487b26
Cédric Ronvel <cedric.ronvel@gmail.com>
2017-01-17 19:05:44 +0100

    Sphere3D LQ interaction OK, HQ NOK

--- sample/ball-test.js
+++ sample/ball-test.js
@@ -8,7 +8,9 @@ console.log( game ) ;
 
 //*
 game.entity.ball.boundVector.position.z = 10 ;
-game.entity.ball.boundVector.vector.y = 10 ;
+//game.entity.ball.boundVector.vector.y = 10 ;
+game.entity.player.boundVector.position.z = 8 ;
+//game.entity.player.boundVector.position.y = 0.01 ;
 //*/
 
 /*
@@ -18,7 +20,7 @@ game.entity.ball.boundVector.vector.y = 10 ;
 
 console.log( game.entity.ball.boundVector ) ; //, game.entity.ball.accelVector ) ;
 
-for ( var i = 0 ; i < 48 ; i ++ )
+for ( var i = 0 ; i < 20 ; i ++ )
 {
 	game.update() ;
 	console.log( '#' + i + ':' , game.entity.ball.boundVector ) ; //, game.entity.ball.accelVector ) ;
