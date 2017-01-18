ee186e0c1566d5ab26a097b2212e35648bae0154
Cédric Ronvel <cedric.ronvel@gmail.com>
2017-01-18 18:25:39 +0100

    Epsilon corrections

--- sample/ball-test.js
+++ sample/ball-test.js
@@ -10,7 +10,7 @@ console.log( game ) ;
 game.entity.ball.boundVector.position.z = 10 ;
 //game.entity.ball.boundVector.vector.y = 10 ;
 game.entity.player.boundVector.position.z = 8 ;
-//game.entity.player.boundVector.position.y = 0.01 ;
+game.entity.player.boundVector.position.y = 0.001 ;
 //*/
 
 /*
@@ -20,7 +20,7 @@ game.entity.ball.boundVector.vector.y = 10 ;
 
 console.log( game.entity.ball.boundVector ) ; //, game.entity.ball.accelVector ) ;
 
-for ( var i = 0 ; i < 22 ; i ++ )
+for ( var i = 0 ; i <= 160 ; i ++ )
 {
 	game.update() ;
 	console.log( '#' + i + ':' , game.entity.ball.boundVector ) ; //, game.entity.ball.accelVector ) ;
