91d1a21b16093c277b3e236eaf418d871b877de3
Cédric Ronvel <cedric.ronvel@gmail.com>
2017-01-21 17:09:26 +0100

    Brush overlapping detection: seems ok!

--- sample/shape-test.js
+++ sample/shape-test.js
@@ -3,9 +3,11 @@ var physic = require( '../lib/physics/physic.js' ) ;
 
 
 cubePos = physic.Vector3D( 0 , 0 , 0 ) ;
+boxPos = physic.Vector3D( 0 , 0 , 0 ) ;
 dotPos = physic.Vector3D( 0 , 0 , 0 ) ;
 
 var cubeShape = physic.Shape.Box.create( 2 , 2 , 2 ) ;
+var boxShape = physic.Shape.Box.create( 3 , 4 , 5 ) ;
 var dotShape = physic.Shape.Dot.create() ;
 
 /*
@@ -13,7 +15,11 @@ console.log( cubeShape ) ;
 console.log( dotShape ) ;
 //*/
 
-console.log( cubeShape.isOverlapping( cubePos , dotShape , dotPos ) ) ;
+//console.log( cubeShape.isOverlapping( cubePos , dotShape , dotPos ) ) ;
 
 dotPos.set( 3 , 0 , 0 ) ;
-console.log( cubeShape.isOverlapping( cubePos , dotShape , dotPos ) ) ;
+//console.log( cubeShape.isOverlapping( cubePos , dotShape , dotPos ) ) ;
+
+
+boxPos.set( 2.4 , 0 , 0 ) ;
+console.log( cubeShape.isOverlapping( cubePos , boxShape , boxPos ) ) ;
