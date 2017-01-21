387a2389cccbbf5e6ec03032a1506b779b519dda
Cédric Ronvel <cedric.ronvel@gmail.com>
2017-01-21 17:14:05 +0100

    tests

--- sample/shape-test.js
+++ sample/shape-test.js
@@ -2,9 +2,9 @@
 var physic = require( '../lib/physics/physic.js' ) ;
 
 
-cubePos = physic.Vector3D( 0 , 0 , 0 ) ;
-boxPos = physic.Vector3D( 0 , 0 , 0 ) ;
-dotPos = physic.Vector3D( 0 , 0 , 0 ) ;
+var cubePos = physic.Vector3D( 0 , 0 , 0 ) ;
+var boxPos = physic.Vector3D( 0 , 0 , 0 ) ;
+var dotPos = physic.Vector3D( 0 , 0 , 0 ) ;
 
 var cubeShape = physic.Shape.Box.create( 2 , 2 , 2 ) ;
 var boxShape = physic.Shape.Box.create( 3 , 4 , 5 ) ;
@@ -21,5 +21,9 @@ dotPos.set( 3 , 0 , 0 ) ;
 //console.log( cubeShape.isOverlapping( cubePos , dotShape , dotPos ) ) ;
 
 
-boxPos.set( 2.4 , 0 , 0 ) ;
+//boxPos.set( 2.4 , 0 , 0 ) ;
+boxShape = physic.Shape.Box.create( 5 , 5 , 5 ) ;
+console.log() ;
 console.log( cubeShape.isOverlapping( cubePos , boxShape , boxPos ) ) ;
+console.log() ;
+console.log( boxShape.isOverlapping( boxPos , cubeShape , cubePos ) ) ;
