c531b24b5cac4f31e2b1e4479a52f5d0ba18a3fe
Cédric Ronvel <cedric.ronvel@gmail.com>
2017-01-22 16:28:03 +0100

    dynamic vertices: ok!

--- sample/shape-test.js
+++ sample/shape-test.js
@@ -6,11 +6,29 @@ var cubePos = physic.Vector3D( 0 , 0 , 0 ) ;
 var boxPos = physic.Vector3D( 0 , 0 , 0 ) ;
 var dotPos = physic.Vector3D( 0 , 0 , 0 ) ;
 var spherePos = physic.Vector3D( 0 , 0 , 0 ) ;
+var sphere2Pos = physic.Vector3D( 0 , 0 , 0 ) ;
 
 var cubeShape = physic.Shape.Box.create( 2 , 2 , 2 ) ;
 var boxShape = physic.Shape.Box.create( 3 , 4 , 5 ) ;
 var dotShape = physic.Shape.Dot.create() ;
 var sphereShape = physic.Shape.Sphere.create( 2 ) ;
+var sphere2Shape = physic.Shape.Sphere.create( 1 ) ;
+
+//*
+spherePos.set( 2 , 1 , 0 ) ;
+console.log() ;
+console.log( sphereShape.isOverlapping( spherePos , sphere2Shape , sphere2Pos ) ) ;
+console.log() ;
+console.log( sphere2Shape.isOverlapping( sphere2Pos , sphereShape , spherePos ) ) ;
+//*/
+
+/*
+spherePos.set( -2 , -1 , -1 ) ;
+console.log() ;
+console.log( cubeShape.isOverlapping( cubePos , sphereShape , spherePos ) ) ;
+console.log() ;
+console.log( sphereShape.isOverlapping( spherePos , cubeShape , cubePos ) ) ;
+//*/
 
 /*
 console.log( cubeShape ) ;
@@ -33,12 +51,5 @@ console.log() ;
 console.log( boxShape.isOverlapping( boxPos , cubeShape , cubePos ) ) ;
 //*/
 
-//*
-spherePos.set( 1 , 0 , 0 ) ;
-console.log() ;
-console.log( cubeShape.isOverlapping( cubePos , sphereShape , spherePos ) ) ;
-console.log() ;
-console.log( sphereShape.isOverlapping( spherePos , cubeShape , cubePos ) ) ;
-//*/
 
 
