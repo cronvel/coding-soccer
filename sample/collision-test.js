582d9bf9f9e870b2550da6bdc79accc0cc505668
Cédric Ronvel <cedric.ronvel@gmail.com>
2017-02-03 19:29:42 +0100

    wip

--- sample/collision-test.js
+++ sample/collision-test.js
@@ -7,12 +7,14 @@ var boxPos = physic.Vector3D( 0 , 0 , 0 ) ;
 var dotPos = physic.Vector3D( 0 , 0 , 0 ) ;
 var spherePos = physic.Vector3D( 0 , 0 , 0 ) ;
 var sphere2Pos = physic.Vector3D( 0 , 0 , 0 ) ;
+var cylinderPos = physic.Vector3D( 0 , 0 , 0 ) ;
 
 var cubeShape = physic.Shape.Box.create( 2 , 2 , 2 ) ;
 var boxShape = physic.Shape.Box.create( 3 , 4 , 5 ) ;
 var dotShape = physic.Shape.Dot.create() ;
 var sphereShape = physic.Shape.Sphere.create( 2 ) ;
 var sphere2Shape = physic.Shape.Sphere.create( 1 ) ;
+var cylinderShape = physic.Shape.Cylinder.create( { x: 0, y: 0, z: 1 } , 1.5 ) ;
 
 
 /*
@@ -34,6 +36,15 @@ console.log( sphereShape.getCollision( spherePos , cubeShape , cubePos ) ) ;
 //*/
 
 
+//*
+spherePos.set( 2 , 2 , 0 ) ;
+console.log() ;
+console.log( cylinderShape.getCollision( cylinderPos , sphereShape , spherePos ) ) ;
+console.log() ;
+console.log( sphereShape.getCollision( spherePos , cylinderShape , cylinderPos ) ) ;
+//*/
+
+
 /*
 dotPos.set( 0.8 , 0.3 , 0 ) ;
 console.log( cubeShape.getCollision( cubePos , dotShape , dotPos ) ) ;
@@ -41,7 +52,7 @@ console.log( dotShape.getCollision( dotPos , cubeShape , cubePos ) ) ;
 //*/
 
 
-//*
+/*
 boxShape = physic.Shape.Box.create( 0.5 , 0.5 , 0.5 ) ;
 boxPos.set( 0.5 , 0.5 , 0.51 ) ;
 console.log() ;
